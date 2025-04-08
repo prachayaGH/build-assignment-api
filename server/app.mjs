0b25fd6a44a1874ff9ec2c6c07e2c2ec85fa0068
Suriya <true.many.032@gmail.com>
2025-04-08 15:46:32 +0700

    Assignment 1

--- server/app.mjs
+++ server/app.mjs
@@ -1,10 +1,50 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
 const port = 4001;
 
+app.use(express.json());
+
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
+});
+
+app.post("/assignments", async (req, res) => {
+  const newAssignment = {
+    ...req.body,
+    created_at: new Date(),
+    updated_at: new Date(),
+    published_at: new Date(),
+  };
+  if (!newAssignment.title || !newAssignment.content || !newAssignment.category) {
+    return res.status(400).json({ message: "Server could not create assignment because there are missing data from client" });
+  }
+  try {
+    await connectionPool.query(
+      `insert into assignments (user_id, title, status, content, category, created_at, updated_at, published_at)
+      values ($1, $2, $3, $4, $5, $6, $7, $8)`,
+      [
+        4,
+        newAssignment.title,
+        newAssignment.status,
+        newAssignment.content,
+        newAssignment.category,
+        newAssignment.created_at,
+        newAssignment.updated_at,
+        newAssignment.published_at
+      ]
+    );
+  } catch (error) {
+    return res
+      .status(500)
+      .json({
+        error: error.message,
+        message:
+          "Server could not create assignment because database connection",
+      });
+  }
+  return res.status(201).json({ message: "Created assignment sucessfully" });
 });
 
 app.listen(port, () => {
